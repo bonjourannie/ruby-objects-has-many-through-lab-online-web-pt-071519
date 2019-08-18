73d8b461e77e9c0e663054e95f101dde484e6c8e
Annie Truex <artruex@gmail.com>
2019-08-18 02:40:37 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,7 +17,11 @@ class Patient
   end
   
   def appointments 
-    
+        Appointment.all.select { |appointment| appointment.patient == self }
+  end
+
+  def doctors
+    appointments.map(&:doctor)
   end 
   
 end 
